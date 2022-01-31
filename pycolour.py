52d54ac53a3e1ae0ad928b51622a1dadd447d656
Georgios Zervakis <zervakisgiorgos91@gmail.com>
2022-01-31 19:35:34 +0000

    Create pycolour.py

---
+++ pycolour.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+#source: https://github.com/MuirlandOracle/CVE-2019-15107/blob/main/CVE-2019-15107.py
+
+
+class colours():
+    red = "\033[91m"
+    green = "\033[92m"
+    blue = "\033[34m"
+    orange = "\033[33m"
+    purple = "\033[35m"
+    end = "\033[0m"
+
+
+banner = (f"""{colours.orange}
+	__        __   _               _         ____   ____ _____ 
+	\ \      / /__| |__  _ __ ___ (_)_ __   |  _ \ / ___| ____|
+	 \ \ /\ / / _ \ '_ \| '_ ` _ \| | '_ \  | |_) | |   |  _|  
+	  \ V  V /  __/ |_) | | | | | | | | | | |  _ <| |___| |___ 
+	   \_/\_/ \___|_.__/|_| |_| |_|_|_| |_| |_| \_\\____|_____|
+						{colours.purple}@Georgios_zm
+		{colours.end}""")
+
+print(banner)
